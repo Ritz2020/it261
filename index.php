a475ceedd8c796b2b8ec02c657e1c8dfef5b5761
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-02-05 20:56:26 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <ul>
                 <li><a href="website/daily.php">Switch</a></li>
                 <li><a href="website/adder.php">Troubleshoot</a></li>
-                <li><a href="">Calculator</a></li>
+                <li><a href="website/calculator/calculator3.php">Calculator</a></li>
                 <li><a href="">Email</a></li>
                 <li><a href="">Database</a></li>
                 <li><a href="">Gallery</a></li>
@@ -70,6 +70,7 @@
             <li><a href="weeks/week4/form3.php ">form3.php</a></li>
             <li><a href="weeks/week4/arithmetic-form.php ">arithmetic-form.php</a></li>
             <li><a href="weeks/week4/celcius.php ">celcius.php</a></li>
+            <li><a href="weeks/week4/test.php ">test.php</a></li>
             
 
         </ol>
@@ -77,10 +78,11 @@
         <h3>Week 5</h3>
 
         <ol>
-            <li><a href=" ">xxxx.php</a></li>
-            <li><a href=" ">xxxx.php</a></li>
-            <li><a href=" ">xxxx.php</a></li>
-            <li><a href=" ">xxxx.php</a></li>
+            <li><a href="weeks/week5/currency1.php ">currency1.php</a></li>
+            <li><a href="weeks/week5/currency2.php ">currency2.php</a></li>
+            <li><a href="weeks/week5/currency3.php ">currency3.php</a></li>
+            <li><a href="weeks/week5/currency4.php ">currency4.php</a></li>
+            <li><a href="weeks/week5/null.php ">null.php</a></li>
 
         </ol>
         </div>
