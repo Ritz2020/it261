15120f18d1d6a6a16994bc18199ddb10476e00d2
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-02-18 15:41:28 -0800

    Add files via upload

---
+++ website/website/contact.php
@@ -0,0 +1,33 @@
+<?php 
+include('config.php');
+include('./includes/header.php'); ?>
+        
+    <div id="wrapper">
+       
+        <main>
+            <h1>
+                Welcome to our Contact Page!
+            </h1>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum tellus sit amet ligula blandit, sed suscipit eros sodales. Cras id orci sit amet augue lobortis molestie. Duis dictum semper varius. Aliquam malesuada erat felis, vitae porttitor felis interdum quis. Mauris mauris erat, porta sed nisi vel, faucibus maximus nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent libero nisl, commodo id malesuada ut, congue et purus. Curabitur bibendum ullamcorper condimentum. Aliquam sit amet urna venenatis elit imperdiet facilisis. </p>
+            
+        
+        
+        </main>
+
+        <aside>
+            <h3>We'd love to hear from you!</h3>
+            
+           <?php
+        include('./includes/form.php'); ?>
+
+           
+
+        </aside>
+
+    </div>
+    <!-- end of wrapper -->
+   
+    <?php
+    include('./includes/footer.php');
+    ?>
---
+++ website/website/thx.php
@@ -0,0 +1,36 @@
+<?php 
+include('config.php');
+include('./includes/header.php'); ?>
+        
+    <div id="wrapper">
+       
+        <main>
+           
+            <h1 style ="color:orange;" >
+                Thank you for filling out our form!
+            </h1>
+            <img style= "margin:15px auto;" src="images/galaxy.jpg" alt="Galaxy">
+            <h3 style ="color:orange;" >
+                Your feedback is important!
+            </h3>
+            
+            
+        
+        
+        </main>
+
+        <aside>
+            <h3>Please keep following our updates!</h3>
+            
+          
+
+           
+
+        </aside>
+
+    </div>
+    <!-- end of wrapper -->
+   
+    <?php
+    include('./includes/footer.php');
+    ?>
