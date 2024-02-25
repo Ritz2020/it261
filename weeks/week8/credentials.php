2c0fe44a763c04aae3a97b64351ea043993e7845
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-02-25 10:11:38 -0800

    Add files via upload

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,7 @@
+<?php // my credentials that will allow me to connect to the database!!!
+
+
+define('DB_NAME', 'rmjmgyoc_it261');
+define('DB_USER','rmjmgyoc_it261_user');
+define('DB_PASSWORD', '');
+define('DB_HOST','localhost');
